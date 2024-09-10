96ec27ee8fc8fd2778764b40cd1956470e80f56d
prashantforsure <prashantforsure@gmail.com>
2024-09-10 07:41:02 +0530

    added stream

---
+++ src/app/api/streams/route.ts
@@ -0,0 +1,132 @@
+import { prisma } from "@/app/lib/db";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const checkStreamSchema = z.object({
+creatorId: z.string(),
+url: z.string()
+});
+export async function POST(req: NextRequest){
+   try{
+        const session  = await getServerSession();
+        const user = await prisma.user.findFirst({
+            where:{
+                email: session?.user?.email ?? ""
+            }
+        })
+        if(!user){
+            return NextResponse.json({
+                message: "user does not exist"
+            })
+        }
+      
+    
+    const data =  checkStreamSchema.parse(req.json());
+    if(!data.url.trim()){
+        return NextResponse.json({
+            message: "the url is empty"
+        })
+    }
+    const extractedId = data.url.split("?v=")[1];
+    const res = await youtubesearchapi.GetVideoDetails(extractedId);
+
+ if(user.id !== data.creatorId){
+    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+    const twoMinutesAgo = new Date(Date.now() - 1 * 60 * 1000);
+    const userRecentStream = await prisma.stream.count({
+        where: {
+            userId: data.creatorId,
+            addedBy: user.id,
+            createAt: {
+                gte : thirtyMinutesAgo
+            }
+        }
+    })
+    
+    const duplicateSong = await prisma.stream.findFirst({
+        where:{
+            userId: data.creatorId,
+            extractedId: extractedId,
+            createAt: {
+                gte: thirtyMinutesAgo
+            }
+        }
+    }) 
+if(!duplicateSong){
+    return NextResponse.json({
+        message: "song was added 30 minutes ago"
+    }, {
+        status: 429
+    })
+}
+const streamsLastTwoMinutes = await prisma.stream.count({
+    where: {
+        userId: data.creatorId,
+        addedBy: user.id,
+        createAt: {
+            gte: twoMinutesAgo
+        }
+    }
+})
+if(streamsLastTwoMinutes >= 2){
+return NextResponse.json({
+    messaage: "Rate limit exceeded: You can only add 2 songs per 2 minutes"
+}, {
+    status: 429
+})
+}
+
+if (userRecentStream >= 5) {
+    return NextResponse.json({
+        message: "Rate limit exceeded: You can only add 5 songs per 10 minutes"
+    }, {
+        status: 429
+    });
+}
+}
+
+const existingUser = await prisma.stream.count({
+    where: {
+        userId: data.creatorId,
+        played: false
+    }
+})
+
+if(existingUser >= 20){
+    return NextResponse.json({
+        message: "queue is full"
+    }, {
+        status: 429
+    })
+}
+const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
+
+const stream = await prisma.stream.create({
+    data: {
+        userId: data.creatorId,
+        addedBy: user.id,
+        url: data.url,
+        extractedId,
+        type: "Youtube",
+        title: res.title ?? "Can't find video",
+        smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg",
+        bigImg: thumbnails[thumbnails.length - 1].url ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
+    }
+})
+return NextResponse.json({
+    ...stream,
+    hasUpvoted: false,
+    upvotes: 0
+})
+   }catch(error){
+    console.log(error);
+    return NextResponse.json({
+        
+    })
+   }
+
+}
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,11 @@
-import { prismaClient } from "@/app/lib/db";
+import { prisma } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse){
 const session = await getServerSession()
 try{
-const user = await prismaClient.user.findFirst({
+const user = await prisma.user.findFirst({
 where: {
      email: session?.user?.email ?? ""
 }
@@ -21,7 +21,8 @@ return NextResponse.json({user})
 }catch(error){
     console.log(error);
     NextResponse.json({
-        error: "there is an error"
+
+        error: "there is an erroree"
     })
 }
 }
